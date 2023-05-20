e45894669f2d5e09f20dbc2cbc06b99fb1f7a0eb
sozud <122322823+sozud@users.noreply.github.com>
2023-05-20 12:13:25 -0700

    Try again

--- scripts/compile_dosemu/compile_docker.sh
+++ scripts/compile_dosemu/compile_docker.sh
@@ -7,7 +7,7 @@ cp  ./rust-dis/output/output.c ./temp/
 cp -r ./rust-dis/output/funcs ./temp/
 cp  ./scripts/compile_dosemu/compile_dosemu.sh ./temp/
 chmod +x ./temp/compile_dosemu.sh
-mkdir -p ./.dosemu
+mkdir -p ./temp/.dosemu
 
 # execute in docker
 docker run --rm -v $(pwd)/:/pwd -w /pwd/temp dosemu:latest /bin/bash -c "./compile_dosemu.sh"
