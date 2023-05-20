c74097b8f5dd9b3038fc8c74a3104378291ac9c4
sozud <122322823+sozud@users.noreply.github.com>
2023-05-20 12:25:33 -0700

    Try again

--- scripts/compile_dosemu/compile_dosemu.sh
+++ scripts/compile_dosemu/compile_dosemu.sh
@@ -1,6 +1,6 @@
 # dosemu commands
 
 # build object
-HOME="." dosemu -quiet -dumb -f ./dosemurc -K . -E "GCC.EXE -c -O2 -m2 -fsigned-char output.c -o output.o"
+HOME="." dosemu -dumb -f ./dosemurc -K . -E "GCC.EXE -c -O2 -m2 -fsigned-char output.c -o output.o"
 # build asm for debugging
-HOME="." dosemu -quiet -dumb -f ./dosemurc -K . -E "GCC.EXE -c -O2 -m2 -fsigned-char -S output.c -o output.s"
+HOME="." dosemu -dumb -f ./dosemurc -K . -E "GCC.EXE -c -O2 -m2 -fsigned-char -S output.c -o output.s"
